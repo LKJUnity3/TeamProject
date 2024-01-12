f0fc29a6e944e847f2eb363826addfb6e24eb550
baekwinter <kksbiy0012@gmail.com>
2024-01-12 14:06:07 +0900

    02:05

--- Player.cs
+++ Player.cs
@@ -73,5 +73,69 @@ namespace TeamProject
             Console.Write(">>> ");
             player.Name = Console.ReadLine();
         }
+
+        public void GetJob()
+        {
+            Console.Clear();
+            Console.WriteLine("캐릭터를 선택하세요.");
+            Console.WriteLine("1. 김유신(검사)");
+            // 각 캐릭터를 설정할 시 , 캐릭터에 대한 소개와 넣어보고 싶어요!
+            Console.WriteLine("2. 이성계(궁수)");
+            Console.WriteLine("3. 홍길동(주술사)");
+            Console.WriteLine("4. 허준(약사)");
+
+            bool IsCorrect = int. TryParse(Console.ReadLine(), out int num);
+            //직업 스탯 
+
+            if(IsCorrect && num > 0 && num <= Enum.GetValues(typeof(Job)).Length) 
+            {
+                Job job = (Job)num;
+
+                switch(num)
+                {
+                    case 1:
+                        {
+                            Job = job.ToString();
+                            lv = 1;
+                            atk = 180;
+                            def = 150;
+                            hp = 250;
+                            Gold = 2000;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Job = job.ToString();
+                            lv = 1;
+                            atk = 150;
+                            def = 200;
+                            hp = 200;
+                            Gold = 2500;
+                            break;
+                        }
+                    case 3:
+                        {
+                            Job = job.ToString();
+                            lv = 1;
+                            atk = 180;
+                            def = 180;
+                            hp = 200;
+                            Gold = 3000;
+                            break;
+                        }
+                    case 4:
+                        {
+                            Job = job.ToString();
+                            lv = 1;
+                            atk = 180;
+                            def = 180;
+                            hp = 200;
+                            Gold = 3000;
+                            break;
+                        }
+
+                }
+            } // 코드가 깃허브에 올라와 있지는 않아서, 확인중
+        }
     }
 }
