800caffe8e2e4898da855cfa29fe3df9e09de4d3
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-01-12 19:16:11 +0900

    damage
    1

---
+++ Damage.cs
@@ -0,0 +1,23 @@
+﻿
+namespace TeamProject
+{
+    public class Damage
+    {
+        public static void PlayerVictim()
+        {
+
+        }
+        public static void EnemyVictim()
+        {
+
+        }
+        public static void ItemEquipe()
+        {
+
+        }
+        public static void SkillDamage()
+        {
+
+        }
+    }
+}
