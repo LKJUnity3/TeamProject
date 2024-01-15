cfd08a1397d353cc5bf421a78de73b78e68cfc43
baekwinter <kksbiy0012@gmail.com>
2024-01-15 20:33:16 +0900

    Dungen,Scene 수정
    Dungen,Scene 수정 했습니다.

--- Scene.cs
+++ Scene.cs
@@ -8,13 +8,14 @@
             STATUS,
             INVENTORY,
             SHOP,
-            DUNGEON
+            DUNGEON,
+            BATTLE
         }
 
         public static void StartScene()
         {
             Console.Clear();
-            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("대한민국에 오신 여러분 환영합니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Console.WriteLine("\n[1] 상태 보기\n[2] 인벤 토리\n[3] 상점\n[4] 던전");
             Console.Write("원하시는 행동을 입력해주세요.\n>>> ");
@@ -28,7 +29,7 @@
                     case (int)SceneType.STATUS: Player.PlayerStatus(); break;
                     case (int)SceneType.INVENTORY: Item.InvenMenu(); break;
                     case (int)SceneType.SHOP: Item.StoreMenu(); break;
-                    case (int)SceneType.DUNGEON: BattleScene.Battle(); break;
+                    case (int)SceneType.DUNGEON: Dungeon.DungeonChoiceMenu(); break;
                     default: StartScene(); break;
                 }
             }
