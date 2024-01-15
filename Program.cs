1f337a019016c0040e687b201dd7ff7a21c77e92
박지훈 <154571034+fgonlygle00@users.noreply.github.com>
2024-01-15 20:56:46 +0900

    1차 메인 화면 사운드 설정

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
-﻿namespace TeamProject
+﻿using WMPLib;
+
+namespace TeamProject
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Task.Run(() => SoundPlayer());
             Player.NickName();
             Player.player.GetJob();
             Skill.SetSkill(Player.player.job);
             Item.ItemSetting();
             Item.PotionSetting();
             Scene.StartScene();
+        }
+
+        static void SoundPlayer()
+        {
+            WindowsMediaPlayer soundMenu = new WindowsMediaPlayer();
+            soundMenu.URL = @"D:\C\C#\TeamProject\sound\mainBGM.mp3";
+            soundMenu.settings.volume = 8;
+            while (true)
+            {
+                soundMenu.controls.play();
+                Thread.Sleep(100);
+            }
         }
     }
 }
